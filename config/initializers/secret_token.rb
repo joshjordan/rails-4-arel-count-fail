546a1cf5923b9cd3251daa12cb9023436cf8b49f
Josh Jordan <josh.jordan@gmail.com>
2014-05-16 14:14:19 -0400

    initial commit on edge Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4ArelCountFail::Application.config.secret_key_base = '293c145d5a36085afe3d69b6a8390924c9740b337caf718d3f7bcc34fc2e3d2b753b6dffd6ed2c008e86d42e995259de6a5475dae65cd42459ae5a8f56e3bca5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4ArelCountFail::Application.config.session_store :cookie_store, key: '_rails-4-arel-count-fail_session'
